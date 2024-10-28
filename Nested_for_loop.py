e82f0f3d389d5c90c2dd3052ec23b600e66410f8
YOGEESH K P <yogeesh793@gmail.com>
2024-10-28 19:45:56 +0530

    Add files via upload

---
+++ Nested_for_loop.py
@@ -0,0 +1,19 @@
+for i in range(1):
+    print("outer loop",i)
+    for j in range(3):
+        print("inner loop",j)
+
+print("----------------------")
+
+for i in range(5):
+    print("outer loop",i)
+    for j in range(3):
+        print("inner loop",j)
+
+print("*************")
+
+for i in ["|","||","|||"]:
+    print("outer loop",i)
+    for j in range(5):
+        print("inner loop",j)
+
